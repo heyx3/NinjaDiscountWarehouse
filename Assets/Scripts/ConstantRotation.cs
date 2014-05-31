dec1c230a88ac90c7690b85dc262e38f66c86e44
heyx3 <manning.w@husky.neu.edu>
2014-05-31 10:11:49 -0400

    Scene improvements

---
+++ Assets/Scripts/ConstantRotation.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+
+/// <summary>
+/// Gives this GameObject a constant rotational velocity.
+/// </summary>
+public class ConstantRotation : MonoBehaviour
+{
+	public Vector3 RotVelocity = Vector3.zero;
+
+	private Transform tr;
+
+	void Awake()
+	{
+		tr = transform;
+	}
+
+	void FixedUpdate()
+	{
+		tr.eulerAngles += RotVelocity * Time.deltaTime;
+	}
+}
