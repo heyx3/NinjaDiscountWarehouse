443341f97d8587b7cda5b2b16556c39e5567b6ea
heyx3 <manning.w@husky.neu.edu>
2014-06-01 12:18:33 -0400

    Auto-aim assist for throwing objects

--- Assets/Scripts/Input/NinjaCluster.cs
+++ Assets/Scripts/Input/NinjaCluster.cs
@@ -9,9 +9,15 @@ using System.Linq;
 [RequireComponent(typeof(PathFollower))]
 public class NinjaCluster : MonoBehaviour
 {
+	public static List<NinjaCluster> AllClusters = new List<NinjaCluster>();
+
+
 	[System.NonSerialized] public List<NinjaAIPlayerInput> NinjaAIs = new List<NinjaAIPlayerInput>();
 
 	public float LerpTowardsNinjasStrength = 0.0f;
+	public float AutoAimHitSphereScale = 1.0f;
+
+	public float MaxNinjaDistance { get; private set; }
 
 	public PathFollower MyPathing { get; private set; }
 
@@ -21,30 +27,52 @@ public class NinjaCluster : MonoBehaviour
 
 	void Awake()
 	{
-		MyPathing = GetComponent<PathFollower>();
+		AllClusters.Add(this);
 
+		MaxNinjaDistance = 0.0f;
+
+		MyPathing = GetComponent<PathFollower>();
 		MyPathing.OnPathEnd += (s, e) =>
 			{
 				MyPathing.enabled = false;
 			};
 	}
 
+	void OnDestroy()
+	{
+		if (!AllClusters.Remove(this))
+			throw new UnityException("This NinjaCluster was removed from the global list before its destruction!");
+	}
+
+	void OnDrawGizmos()
+	{
+		Gizmos.color = new Color(0.2f, 1.0f, 0.2f, 0.4f);
+		Gizmos.DrawSphere(transform.position, MaxNinjaDistance);
+	}
+
 	void Update()
 	{
+		//If this cluster is empty, destroy it.
 		if (NinjaAIs.Count == 0)
 		{
 			Destroy(gameObject);
 			return;
 		}
 
-		if (NinjaAIs.Count > 0)
+		//Lerp towards the ninja's collective center a bit.
+		//Also calculate the radius of this cluster's auto-aim hit sphere.
+		Vector3 avgPos = Vector3.zero;
+		float maxDist = 0.0f;
+		foreach (NinjaAIPlayerInput ninja in NinjaAIs)
 		{
-			Vector3 avgPos = Vector3.zero;
-			foreach (NinjaAIPlayerInput ninja in NinjaAIs)
-				avgPos += ninja.MyTransform.position;
-			avgPos /= NinjaAIs.Count;
+			avgPos += ninja.MyTransform.position;
 
-			MyPathing.MyTransform.position = Vector3.Lerp(MyPathing.MyTransform.position, avgPos, LerpTowardsNinjasStrength);
+			float tempDist = (MyPathing.MyTransform.position - ninja.MyTransform.position).sqrMagnitude;
+			if (tempDist > maxDist)
+				maxDist = tempDist;
 		}
+		avgPos /= NinjaAIs.Count;
+		MyPathing.MyTransform.position = Vector3.Lerp(MyPathing.MyTransform.position, avgPos, LerpTowardsNinjasStrength);
+		MaxNinjaDistance = AutoAimHitSphereScale * Mathf.Sqrt(maxDist);
 	}
 }
